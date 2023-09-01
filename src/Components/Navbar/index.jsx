bae5bf2a660f3588d46feed47436946757fdd35c
dwbrown115 <114196159+dwbrown115@users.noreply.github.com>
2023-09-01 05:55:39 -0500

    continue work on navbar

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,10 +1,45 @@
 import React from "react";
 
-import "./navbar.scss"
+import "./navbar.scss";
 function Navbar() {
   return (
-    <div>
-      <h1>Navbar</h1>
+    <div className="Navbar">
+      <div className="NavbarElements">
+        <div className="Logo">
+          <img
+            className="LogoImage"
+            src="./silverCastleDigitalLogo2.png"
+            alt="silverCastleDigitalLogo.png"
+          />
+        </div>
+        <div className="NavbarElementWrapper">
+          {/* <div className="NavbarElement">
+            <div className="SeparatorBefore" />
+            <div className="Element">Home</div>
+            <div className="Separator" />
+          </div> */}
+          <div className="NavbarElement">
+            <div className="SeparatorBefore" />
+            <div className="Element">About</div>
+            <div className="Separator" />
+          </div>
+          <div className="NavbarElement">
+            <div className="SeparatorBefore" />
+            <div className="Element">Services</div>
+            <div className="Separator" />
+          </div>
+          <div className="NavbarElement">
+            <div className="SeparatorBefore" />
+            <div className="Element">Projects</div>
+            <div className="Separator" />
+          </div>
+          <div className="NavbarElement">
+            <div className="SeparatorBefore" />
+            <div className="Element">Contact</div>
+            <div className="Separator" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
