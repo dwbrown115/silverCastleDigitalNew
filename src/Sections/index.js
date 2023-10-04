4462899903e1c194847d0ae372a66cf7eb6fdf4d
dwbrown115 <114196159+dwbrown115@users.noreply.github.com>
2023-10-04 05:29:47 -0500

    forgot to remove section of code relating to depriciated team section

--- src/Sections/index.js
+++ src/Sections/index.js
@@ -2,4 +2,4 @@ export { default as Contact } from './Contact';
 export { default as Hero } from './Hero';
 export { default as Projects } from './Projects';
 export { default as Services } from './Services';
-export { default as Team } from './Team';
+// export { default as Team } from './Team';
